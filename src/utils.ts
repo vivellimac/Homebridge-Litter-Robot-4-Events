28003caecda2ff3cad1bcc71e93d963ab3b3265b
vivellimac <114693343+vivellimac@users.noreply.github.com>
2025-09-27 19:59:23 -0400

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,9 @@
-export function parseJwt (token) {
-  return JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString());
+export function parseJwt(token: string): Record<string, unknown> {
+  try {
+    const b64 = (token.split('.')[1] || '').replace(/-/g, '+').replace(/_/g, '/');
+    const padded = b64 + '='.repeat((4 - (b64.length % 4)) % 4);
+    return JSON.parse(Buffer.from(padded, 'base64').toString('utf8'));
+  } catch {
+    return {};
+  }
 }
