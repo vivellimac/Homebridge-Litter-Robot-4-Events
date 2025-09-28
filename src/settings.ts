d2ea164a0f899ed5d0e2d3d314aa18a17eb3c39d
vivellimac <114693343+vivellimac@users.noreply.github.com>
2025-09-28 01:21:17 -0400

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'LitterRobot4';
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-litter-robot-4';
+export const PLUGIN_NAME = 'homebridge-litter-robot-4-events';
